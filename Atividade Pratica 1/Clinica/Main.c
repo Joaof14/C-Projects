d8210c40747c156d9e5ca1f4360ec6613cf9f2ec
Frederico Gregório <fredd.gregorio@gmail.com>
2025-05-22 15:06:20 -0300

    Update Main.c

---
+++ Atividade Pratica 1/Clinica/Main.c
@@ -0,0 +1,13 @@
+// Criação de Switch Case para escolher qual parametro:
+// Opções:
+// Ir Para Medico -------- 
+// Ir Para Paciente ------
+// Ir Para Consulta ------
+// Ir Para Relatórios ----
+
+#include "Medico.h"
+#include "Consulta.h"
+#include "Paciente.h"
+#include "Relatorios.h"
+
+
