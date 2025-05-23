dbc9d5f4d8075fe14709ed016f716f632541b70c
Frederico Gregório <fredd.gregorio@gmail.com>
2025-05-22 21:17:44 -0300

    atualização

--- Atividade Pratica 1/Clinica/Relatorios.c
+++ Atividade Pratica 1/Clinica/Relatorios.c
@@ -1,1 +1,43 @@
 #include "Relatorios.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+    
+{
+    int opcao;
+    do
+    {
+        printf("Menu Principal\n");
+        printf("1. Relatório de Consultas (Pacientes)\n");
+        printf("2. Relatório de Consultas (Medicos)\n");
+        printf("3. Relatório de Consulta (Especialidade)\n");
+        printf("4. Relatório de Consulta Dia Atual\n");
+        printf("5. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+                relatorioConsultaPacientes();
+                break;
+            case 2:
+                void relatorioConsultaMedicos();
+                break;
+            case 3:
+                void relatorioConsultaPorEspecialidae();
+                break;
+            case 4:
+               void relatorioConsultaDiaAtual();
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+    } while (opcao != 5);
+
+    return 0;
+}
---
+++ Atividade Pratica 1/Clinica/Relatorios.h
@@ -0,0 +1,10 @@
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
+
+
+void relatorioConsultaPacientes();
+void relatorioConsultaMedicos();
+void relatorioConsultaPorEspecialidae();
+void relatorioConsultaDiaAtual();
+
+#endif
