e25364c7f631d8cf4bdc34090a9d05df45c2c424
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-22 21:48:48 -0300

    ""

--- Atividade Pratica 1/Clinica/Medico.c
+++ Atividade Pratica 1/Clinica/Medico.c
@@ -22,5 +22,32 @@ void atualizarMedico(){
 }
 
 void menuMedico(){
-    
+    int opcao;
+    do
+    {
+        printf("Menu Principal\n");
+        printf("1. Cadastrar Paciente\n");
+        printf("2. Atualizar Paciente\n");
+        printf("3. Remover Paciente\n");
+        printf("4. Voltar\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+                cadastrarPaciente();
+                break;
+            case 2:
+                atualizarPaciente();
+                break;
+            case 3:
+                removerPaciente();
+                break;
+            case 4:
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+    } while (opcao != 5);
 }
