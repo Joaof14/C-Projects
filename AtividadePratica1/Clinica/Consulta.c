c02ed6f96f97263b21de918097d69fa510529b39
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-24 15:32:13 -0300

    ""

--- AtividadePratica1/Clinica/Consulta.c
+++ AtividadePratica1/Clinica/Consulta.c
@@ -1,6 +1,24 @@
 #include "Relatorios.h"
 #include "Consulta.h"
 
-void menuConsultas(){
+
+void agendarConsulta(){
+
+}
+
+void registrarConsulta(){
+
+}
+
+void cancelarConsulta() {
+
+}
+
+void listarConsultas(){
     
+}
+
+
+void menuConsultas(){
+
 }
