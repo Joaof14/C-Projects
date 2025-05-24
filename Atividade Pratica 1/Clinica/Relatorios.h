3b44510577ebdf7bfb89c554d86c37e44caba947
Frederico Gregório <fredd.gregorio@gmail.com>
2025-05-24 14:10:50 -0300

    Update Relatorios.h

--- Atividade Pratica 1/Clinica/Relatorios.h
+++ Atividade Pratica 1/Clinica/Relatorios.h
@@ -2,9 +2,11 @@
 #define RELATORIOS_H
 
 
+void menuRelatorios();
 void relatorioConsultaPacientes();
 void relatorioConsultaMedicos();
 void relatorioConsultaPorEspecialidae();
 void relatorioConsultaDiaAtual();
+
 
 #endif
