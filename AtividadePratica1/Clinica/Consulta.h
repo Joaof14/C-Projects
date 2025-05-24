5ca854bed430091552d5c516869c264c9996736a
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-24 17:03:28 -0300

    ""

--- AtividadePratica1/Clinica/Consulta.h
+++ AtividadePratica1/Clinica/Consulta.h
@@ -1,6 +1,25 @@
 #include "Relatorios.h"
 
 
+enum statusConsulta { AGENDADA, REALIZADA, CANCELADA
+};
+
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+    int hora;
+    int minuto;
+} DataHora;
+
+typedef struct {
+    int id;
+    char pacienteCPF[11];
+    char medicoCRM[6];
+    DataHora data_hora;
+    enum statusConsulta status;
+} Consulta;
+
 void listarConsultas();
 
 void agendarConsulta();
