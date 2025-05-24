ebbcb305fadb4e69b998a4beb9cc170752a9c00d
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-24 14:21:09 -0300

    "Codigo"

--- Atividade Pratica 1/Clinica/Main.c
+++ Atividade Pratica 1/Clinica/Main.c
@@ -12,5 +12,37 @@
 #include "Consulta.h"
 #include "Relatorios.h"
 
+int main (){
+    int n;
+    do {
+        switch (n)
+        {
+        case 1:
+            menuMedico();
+            break;
+            
+        case 2:
+        menuPaciente();
+            /* code */
+            break;
+        case 3:
+        menuRelatorios();
+            /* code */
+            break;
+        case 4:
+        menuConsultas();
+            /* code */
+            break;
+        
+        default:
+        printf("Opção inválida");
+            break;
+        }
+        
+    }
+    while (n != 0);
+    
+}
+
 
 
