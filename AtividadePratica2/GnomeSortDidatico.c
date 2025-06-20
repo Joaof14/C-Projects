e50f7a1f12e8d281bdbb73afa62fc5b475563562
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-20 00:33:42 -0300

    ""

---
+++ AtividadePratica2/GnomeSortDidatico.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <time.h>
+#include "sort.h"
+
+
+//Funções para ensino didático: escreverVetor, stupidSort, e Exemplo1
+void escreverVetor(int * vetor, int tamanho, int pos){
+    FILE * arquivo = fopen("GnomeSort.txt", "a");
+    fprintf(arquivo, "Posição nessa iteração: %d.    Vetor: ", pos);
+     for (int i = 0; i < tamanho; i++) {
+        fprintf(arquivo, "%d ", vetor[i]);
+    }
+    fprintf(arquivo, "\n"); 
+    
+    fclose(arquivo);
+
+}
+
+void stupidSort(int * vetor, int tamanho){
+    //Declara posição e auxiliar
+    int pos = 0, aux;
+    //loop que atualiza posição e faz troca se necessário
+    while(pos < tamanho){
+        escreverVetor(vetor, tamanho, pos);
+        if (pos == 0 || vetor[pos] >= vetor[pos-1]){
+            pos++;
+        }
+        else{
+            aux = vetor[pos];
+            vetor[pos] = vetor[pos - 1];
+            vetor[pos-1] = aux;
+            pos--;
+        }
+        
+    }
+    
+}
+
+
+int main(){
+    //Criação do arquivo
+    FILE * arquivo = fopen("GnomeSort.txt", "w");
+    if (arquivo == NULL){
+        printf("\nNão foi possível criar arquivo\n");
+        return;
+    }
+
+    int vetor[] = {3,7,5,4,8,5,9,3};
+    int tamanho = sizeof(vetor)/sizeof(int);
+
+    stupidSort(vetor, tamanho);
+    fclose(arquivo);
+}
