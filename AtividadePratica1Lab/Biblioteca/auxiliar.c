36f62484f2d565d10b178ae3d701597d9aacf51a
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-22 15:04:50 -0300

    ""

--- AtividadePratica1Lab/Biblioteca/auxiliar.c
+++ AtividadePratica1Lab/Biblioteca/auxiliar.c
@@ -19,3 +19,26 @@ void criarArquivos(const char *caminho, const char *colunas) {
         fclose(arquivo); 
     }
 }
+
+int ler_entrada_limitada(char *buffer, size_t tamanho_max) {
+    if (fgets(buffer, tamanho_max, stdin) == NULL) {
+        return 0; // Erro ou EOF
+    }
+    
+    size_t len = strlen(buffer);
+    
+    // Se a entrada for muito longa (sem \n no final)
+    if (len == tamanho_max - 1 && buffer[len - 1] != '\n') {
+        // Limpar o buffer de entrada
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        return 0;
+    }
+    
+    // Remover \n se existir
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+    
+    return 1;
+}
