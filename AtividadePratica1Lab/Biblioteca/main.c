9b02db5a5c9ef90a410ac7bc0967d68f2636bec3
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-22 14:47:09 -0300

    "main.c"

--- AtividadePratica1Lab/Biblioteca/main.c
+++ AtividadePratica1Lab/Biblioteca/main.c
@@ -10,12 +10,29 @@
 int main(){
     //Criação dos arquivos que servirão como banco de dados
     criarArquivos("arquivos/livros.txt", "Colunas");
-    criarArquivos("arquivos/Emprestimos.txt", "Colunas");
+    criarArquivos("arquivos/emprestimos.txt", "Colunas");
 
 
     int op;
     do {
-
-    }while(op != 4);
+        printf("Sistema de Gerenciamento de uma biblioteca\n");
+        printf("Escolha a opção que você quer acessar:\n0.Sair\n1.Livros\n2.Empréstimos\n3.Relatórios\n");
+        switch (op)
+        {
+            case 0:
+                printf("Fechando programa\n");
+                break;
+            case 1:
+                MenuLivros();
+                break;
+            case 2:
+                MenuEmprestimos();
+                break;
+            case 3:
+            MenuRelatorios();
+        default:
+            break;
+        }
+    }while(op != 0);
     return 0;
 }
