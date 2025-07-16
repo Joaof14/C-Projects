e583d31003642e92f7f4ccf5b87c4712c4d554f7
Joaof14 <joao-pedro032011@hotmail.com>
2025-07-16 20:44:48 -0300

    "structs"

--- AtividadePratica3/types.h
+++ AtividadePratica3/types.h
@@ -1,20 +1,34 @@
-#ifndef STACKCARDS_H
-#define STACKCARDS_H
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct Cards{
+    char suit;
+    char value;
+} Cards;
 
 typedef struct Node{
-    int data;
+    Cards card;
     struct Node* next;
 } Node;
 
 typedef struct Stack{
-    Node *top;
+    Node * top;
+    int size;
+    
 } Stack;
 
 typedef struct Queue{
-    Node * front;
-    Node * rear;
+    Node * front; //inicio da fila
+    Node * rear; //fim da fila
+    int size;
+    
 } Queue;
 
+typedef struct {
+    char name[50];
+    Stack hand;   //cartas na mão do jogador (pode ser uma pilha ou lista)
+    int points;
+} Player;
 
 
 #endif
